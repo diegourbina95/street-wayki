5251772ec5477bb5aab9caa432c3a98fafab290a
Diego Urbina <diegourbina.dev@gmail.com>
2024-11-17 00:35:25 -0500

    fix: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/street-wayki/",
+  base: "https://diegourbina95.github.io/street-wayki/",
   plugins: [react()],
 });
