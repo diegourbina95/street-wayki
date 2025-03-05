446d7be23da93ea68c170b1f78846041ed1c6d07
Diego Urbina <diegourbina.dev@gmail.com>
2025-03-04 21:09:40 -0500

    SW-58 feature: se implementó letra mayúscula al inicio

--- src/views/public/Home/components/InformationSection.tsx
+++ src/views/public/Home/components/InformationSection.tsx
@@ -1,11 +1,18 @@
 interface InformationSectionProps {
+  initialCapitalLetter?: string;
   text: string;
 }
 
 export const InformationSection: React.FC<InformationSectionProps> = ({
+  initialCapitalLetter,
   text,
 }) => (
   <div className="home-page__information">
-    <p>{text}</p>
+    <p>
+      <span className="home-page__information--capital">
+        {initialCapitalLetter}
+      </span>
+      {text}
+    </p>
   </div>
 );
