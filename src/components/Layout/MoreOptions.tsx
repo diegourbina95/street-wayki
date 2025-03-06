f6c4454f99727705ea3584f64867a40fccc0544b
unknown <dparedes02@gmail.com>
2025-03-06 18:31:08 -0500

    SW-62: fix texto

--- src/components/Layout/MoreOptions.tsx
+++ src/components/Layout/MoreOptions.tsx
@@ -6,23 +6,23 @@ import { ButtonNes } from "@/components/Inputs";
 /* STYLES */
 import "@/styles/more-options.scss";
 
+import imagenReportaje from "@/assets/images/Congresistas-Patrimonio1.jpg";
+
 export const MoreOptions = () => {
   return (
     <div className="more-options">
       <div className="more-options__content">
-        <div className="more-options__item">
-          <div className="more-options__text">
-            Los congresistas Digna Calle (Podemos Perú), Héctor Valer (Somos
-            Perú), María del Carmen Alva (no agrupada), y Jorge Montoya
-            (Renovación Popular) han declarado una reducción de su patrimonio
-            equivalente a entre S/1 millón y más de S/20 millones. Este año,
-            Alva Prieto y Montoya Manrique fueron denunciados
-            constitucionalmente por la Fiscalía de la Nación por peculado y
-            aprovechamiento indebido del cargo, respectivamente. A Calle
-            Lobatón, el Ministerio Público le abrió una investigación preliminar
-            por lavado de activos, mientras que Valer Pinto ha afrontado
-            investigaciones fiscales por corrupción y otros delitos.
+
+      <div className="more-options__img">
+          <img width={"400"} src={imagenReportaje} alt="reportaje" />
+      </div>
+      <div className="more-options__item">
+          
+          
+      <div className="more-options__title">
+      Patrimonio en fuga: congresistas se desprenden de sus bienes
           </div>
+        
           
             <ButtonNes text="LEER REPORTAJE COMPLETO" href="https://ojo-publico.com/5515/patrimonio-fuga-congresistas-se-desprenden-sus-bienes" />
           
